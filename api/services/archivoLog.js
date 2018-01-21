995a7098f63fb44e7f60993774cac35245023985
joselimaico <joselimaicotele@gmail.com>
2018-01-20 21:34:50 -0500

    first commit

---
+++ api/services/archivoLog.js
@@ -0,0 +1,35 @@
+module.exports ={
+
+    escribirLog: function(operacion){
+        var fs = require('fs');
+            var moment = require("moment");
+            fs.appendFileSync("log.txt", ">>Start "+operacion+" at "+ moment().format() + "\n", "UTF-8",{'flags':'a+'} );
+
+            
+            const{ exec }  = require('child_process');
+            exec('free', (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                    return;
+                }
+  
+                fs.appendFileSync("log.txt",`${stdout}`,"UTF-8",{'flags':'a+'});
+                //fs.appendFileSync("log.txt",">>end CREATE at " + moment().format()+"\n","UTF-8",{'flags':'a+'});
+            });
+            
+            exec('uptime', (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                    return;
+                }
+  
+                fs.appendFileSync("log.txt",`${stdout}`,"UTF-8",{'flags':'a+'});
+               // fs.appendFileSync("log.txt",">>end CREATE at " + moment().format()+"\n","UTF-8",{'flags':'a+'});
+            });
+
+            var hrTime = process.hrtime();
+            var t_final =(hrTime[0] * 1000000 + hrTime[1] / 1000);
+            fs.appendFileSync("log.txt","tiempo total "+t_final+ " microsegundos \n","UTF-8",{'flags':'a+'});
+
+    }
+}
